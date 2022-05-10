2b14e048fefae65cd1d1884be00a6cfa7c87b103
Karuz <95084763+KaruzG@users.noreply.github.com>
2022-05-10 18:53:50 +0200

    Main Script

---
+++ main.js
@@ -0,0 +1,28 @@
+function onOpen(){ // This just creates a menu with the functions
+  const ui = SpreadsheetApp.getUi();
+  ui.createMenu('Menu')
+    .addItem('Uppercase Selection', 'setSelectedUppercase')
+    .addItem('Lowercase Selection', 'setSelectedLowercase')
+    .addToUi();
+}
+
+
+function setSelectedUppercase() { // Function to uppercase
+  const range = SpreadsheetApp.getActiveSheet()
+                              .getActiveRange();
+
+  const values = range.getDisplayValues()
+                      .map(row => row.map(col => (col) ? col.toUpperCase() : col));
+  
+  range.setValues(values);
+}
+
+function setSelectedLowercase() { // Function to lowercase
+  const range = SpreadsheetApp.getActiveSheet()
+                              .getActiveRange();
+
+  const values = range.getDisplayValues()
+                      .map(row => row.map(col => (col) ? col.toLowerCase() : col));
+  
+  range.setValues(values);
+}
